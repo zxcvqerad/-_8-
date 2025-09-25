278a1eebcf7926a66c4ed2734d2a7349d1012cb0
zxcvqerad <dudnf21123@gmail.com>
2025-09-26 00:38:45 +0900

    2주차 1번쨰 문제 과제
    2주차 1번쨰 문제 과제

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+int score = 84;
+
+if (score >= 90){
+print("이 학생의 점수는 $score점 이며, 등급은 A등급 입니다!");
+}
+else if (score >= 80){
+print("이 학생의 점수는 $score점 이며, 등급은 B등급 입니다!");
+}
+
+else {print("이 학생의 점수는 $score점 이며, 등급은 C등급 입니다!");
+}
+}
